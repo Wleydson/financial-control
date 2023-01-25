a62586766d6589162e328645ef1d0061e08a9cfb
Wleydson <wleydsonredes@gmail.com>
2023-01-25 19:01:31 -0300

    add endpoints resume credit card

---
+++ src/main/java/br/com/wleydson/financialcontrol/resources/dto/CreditCardExtractDTO.java
@@ -0,0 +1,19 @@
+package br.com.wleydson.financialcontrol.resources.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+
+@Getter
+@Setter
+@AllArgsConstructor
+public class CreditCardExtractResumeDTO {
+
+    private Integer size;
+    private BigDecimal value;
+    private String file;
+    private String name;
+
+}
